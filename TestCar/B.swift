6a948908c96ce452fdb9be243b66e86dc43b2548
TerryLee <terry_lee@tutk.com>
2020-08-10 08:50:50 +0800

    init

---
+++ TestCar/B.swift
@@ -0,0 +1,21 @@
+//
+//  B.swift
+//  TestCar
+//
+//  Created by Terry Lee on 2020/8/10.
+//  Copyright © 2020 Terry Lee. All rights reserved.
+//
+
+import Foundation
+
+public class B {
+    let name: String
+    
+    init(name: String) {
+        self.name = name
+    }
+    
+    func printSomething() {
+        print("B is \(name)")
+    }
+}
